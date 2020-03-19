8f369261f769d258c903651afa16de13d1b5215d
叶东宇 <993248739@qq.com>
2020-03-20 00:04:54 +0800

    测试路由，关闭过于严格的代码检测

--- config/index.js
+++ config/index.js
@@ -23,7 +23,8 @@ module.exports = {
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: true,
+    // todo 打开代码规范校验
+    useEslint: false,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
---
+++ src/components/vtk/RouterTest.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>Just test Router</h1>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RouterTest',
+  data () {
+    return {
+      msg: 'router Test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
